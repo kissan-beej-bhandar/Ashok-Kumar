097e129de181a1b90a19b996756b9341df8194e9
Vipin <sttarworld01@gmail.com>
2021-12-07 19:32:18 +0530

    dsa

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Route,Switch } from 'react-router-dom';
+import Home from "./Pages/Home";
+import Prods from './Pages/Prods';
+
+const App = () => {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/Home" component={Home}/>
+        <Route exact path="/Products" component={Prods}/>
+      </Switch>
+    </>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import {HashRouter as Route} from "react-router-dom";
+
+ReactDOM.render(
+  <Route>
+    <App />
+  </Route>,
+  document.getElementById('root')
+);
+
+                 
